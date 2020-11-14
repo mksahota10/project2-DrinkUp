fd56fde7f97d5bfeaf18efec7de8f52e6e876c55
mksahota10 <mksahota10@gmail.com>
2020-11-13 19:39:11 -0800

    some css done

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,20 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const db =require('../models')
-const passport = require('../config/ppConfig.js')
-const axios = require('axios');
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+const passport = require("../config/ppConfig.js");
+const axios = require("axios");
 
-
-router.get('/:drinkId', (req, res)=>{
-    db.userdrinks.findOne({
-      where:{userId: req.user.id, 
-        drinkId: req.params.drinkId},
+router.get("/:drinkId", (req, res) => {
+  db.userdrinks
+    .findOne({
+      where: { userId: req.user.id, drinkId: req.params.drinkId },
     })
-    .then(foundComment=>{
-      console.log('my comment', foundComment)
-      res.render('comments', {newComments: foundComment.dataValues})
-    })
-  })
+    .then((foundComment) => {
+      console.log("my comment", foundComment);
+      res.render("comments", { newComments: foundComment.dataValues });
+    });
+});
 
-
-  module.exports=router
+module.exports = router;
--- controllers/favorites.js
+++ controllers/favorites.js
@@ -1,94 +1,85 @@
-const express = require('express')
-const router = express.Router()
-const db =require('../models')
-const passport = require('../config/ppConfig.js')
-const axios = require('axios');
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+const passport = require("../config/ppConfig.js");
+const axios = require("axios");
 
-
-
-// route to post data to favorites page 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    db.drink.findOrCreate({
-      where:{
+// route to post data to favorites page
+router.post("/", (req, res) => {
+  console.log(req.body);
+  db.drink
+    .findOrCreate({
+      where: {
         name: req.body.name,
-        ingredients:req.body.ingredients,
-        instructions:req.body.instructions,
-        picture:req.body.picture
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        picture: req.body.picture,
       },
     })
-    .then(([drink, created])=>{
-      console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-      drink.addUser(req.user)
-      .then(relationInfo=>{
-        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-        console.log(`${req.user.name} added to ${drink.name}`)
-        console.log(relationInfo)
-       // res.render('/favorites'{})
+    .then(([drink, created]) => {
+      console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+      drink.addUser(req.user).then((relationInfo) => {
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        console.log(`${req.user.name} added to ${drink.name}`);
+        console.log(relationInfo);
+        // res.render('/favorites'{})
         //res.render('favorites', {name:drink.name, ingredients:drink.ingredients, instructions:drink.instructions, picture: drink.picture})
-        res.redirect('/favorites')
-      })
-    }) 
+        res.redirect("/favorites");
+      });
+    })
     .catch((error) => {
-      console.log(error)
-   })
-  })
-//route to find drink associated with user
-    router.get('/', (req, res)=> {
-        console.log("###############################################😞")
-          console.log(req.session)
-      db.user.findOne({
-        where:{id: req.user.id},
-        include:[db.drink]
-        })
-      .then(foundUser=>{
-        console.log("###############################################😞")
-      
-    res.render('favorites', {faveDrinks: foundUser.drinks})
-    console.log(foundUser.drinks)
-    //console.log(drink)
-      })
-     
+      console.log(error);
     });
+});
+//route to find drink associated with user
+router.get("/", (req, res) => {
+  console.log("###############################################😞");
+  console.log(req.session);
+  db.user
+    .findOne({
+      where: { id: req.user.id },
+      include: [db.drink],
+    })
+    .then((foundUser) => {
+      console.log("###############################################😞");
 
+      res.render("favorites", { faveDrinks: foundUser.drinks });
+      console.log(foundUser.drinks);
+      //console.log(drink)
+    });
+});
 
 //Delete a drink frommy Drink (favorites)
-router.delete('/:id', (req, res)=>{
-  db.drink.destroy ({
-    where: {id:req.params.id}
-  })
-  .then(numRowsDeleted=>{
-    console.log(numRowsDeleted)
-    res.redirect('/favorites')
-  })
-  .catch(err=>{
-    res.send(err)
-  })
-})
+router.delete("/:id", (req, res) => {
+  db.drink
+    .destroy({
+      where: { id: req.params.id },
+    })
+    .then((numRowsDeleted) => {
+      console.log(numRowsDeleted);
+      res.redirect("/favorites");
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
-router.put('/:id', (req, res)=>{
-  console.log('😭😭', req.params)
-  console.log('😭😭', req.user.id)
-  db.userdrinks.update(
-    {comment: req.body.comment},
-    {where: {userId:req.user.id, drinkId:req.params.id}
-}).then(newComment=>{
-    console.log('This is my comment', newComment)
-    // newComment.
-    res.redirect(`/comments/ ${req.params.id}`)
-}).catch(err=>{
-  res.send(err)
-})
-})
+router.put("/:id", (req, res) => {
+  console.log("😭😭", req.params);
+  console.log("😭😭", req.user.id);
+  db.userdrinks
+    .update(
+      { comment: req.body.comment },
+      { where: { userId: req.user.id, drinkId: req.params.id } }
+    )
+    .then((newComment) => {
+      console.log("This is my comment", newComment);
+      // newComment.
+      res.redirect(`/comments/ ${req.params.id}`);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
-
-
-
-
-
-
-
-
-
-
-module.exports=router
+module.exports = router;
